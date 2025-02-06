1738a88b322593b4c6133816702a31c24bbc2955
abhay_pandit <abhaypanditzx@gmail.com>
2025-02-06 15:23:18 +0530

    s

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://chat-app-1-crsb.onrender.com/");
 
 function Chat() {
   const [messages, setMessages] = useState([]);
