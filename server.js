6d884283faa55e534c2caac975b5f14dfff413db
abhay pandit <abhaypanditzx@gmail.com>
2026-05-02 15:46:30 +0530

    ui upgrade

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
-const express = require("express");
-const { Server } = require("socket.io");
+const express =  require("express");
+const {Server} =require("socket.io");
 const http = require("http");
-
 const app = express();
-
 const server = http.createServer(app);
 
-const io =  new Server(server,{
-    cors:{
-        origin:"*"
-    }
-});
+ const io =  new Server(server,{
+    cors:"*"
+ })
 
 
-io.on("connection",(socket)=>{
-console.log("user connected",socket.id);
-socket.on("send-message",(msg)=>{
-io.emit("receive-message",msg)
-})
-})
+ io.on("connection",(socket)=>{
+    console.log("user connected:",socket.id);
+    socket.on("s-msg",(msg)=>{
+        console.log(msg);
+        io.emit("r-msg",msg);
+    })
+ })
+const PORT = 5000;
 
-server.listen(5000,(error)=>{
-    if(error){
-        console.log(error)
-    }
-    else{
-        console.log("server started on port 5000")
-    }
-})
+ server.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
+ })
--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,15 @@ import io from "socket.io-client";
 
 export default function Home() {
   const [socket, setSocket] = useState(null);
+  const [username, setUsername] = useState("");
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
 
   useEffect(() => {
-    const newSocket = io("https://chat-app-1-dx36.onrender.com/");
-    console.log(newSocket)
+    // const newSocket = io("https://chat-app-1-dx36.onrender.com/");
+    const newSocket = io("http://localhost:5000");
     setSocket(newSocket);
-    newSocket.on("receive-message", (msg) => {
+    newSocket.on("r-msg", (msg) => {
       setChat((prev) => [...prev, msg]);
     });
 
@@ -21,28 +22,40 @@ export default function Home() {
 
   const sendMessage = () => {
     if (socket && message) {
-      socket.emit("send-message", message);
+      socket.emit("s-msg",{username:username,message:message,senderId:socket.id});
       setMessage("");
     }
   };
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Next Chat 💬</h1>
-
-      <div style={{ border: "1px solid black", height: 300, overflowY: "scroll" }}>
-        {chat.map((msg, i) => (
-          <p className={"bg-pink-500"} key={i}>{msg}</p>
+    <div style={{ padding: 20 }} className=" flex flex-col h-screen w-full" >
+      <h1 className="text-center text-2xl font-bold mb-4">Next Chat 💬</h1>
+      <input type="text" 
+      value={username} 
+      onChange={(e)=>setUsername(e.target.value)} 
+      placeholder="Enter your username"
+      className="border-black border rounded-lg bg-amber-50  mb-3 p-2"/>
+      <div
+     
+        className="overflow-y-scroll  py-6 border border-black rounded-md h-[400px] "
+      >
+        {chat?.map((msg, i) => (
+          <p className={` w-fit mb-2  mx-2 px-6  rounded-lg text-white p-2 ${msg.senderId ===socket.id ? "ml-auto bg-green-500" : "bg-pink-500  "}`} key={i}>
+          <span className=" font-semibold">{msg.username}:</span>{msg.message}
+          </p>
         ))}
       </div>
 
-      <input
+    <div className=" absolute bottom-4 left-4 flex ">
+        <input
         value={message}
         onChange={(e) => setMessage(e.target.value)}
         placeholder="Type..."
+        className="w-full border rounded-l-2xl"
       />
 
       <button onClick={sendMessage}>Send</button>
+    </div>
     </div>
   );
 }
